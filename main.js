48c4a750a45cb866287cfb994f7285ba4622d189
SANDIP CHITALE <sandipchitale@gmail.com>
2017-11-07 12:23:17 -0800

    Support Electron

---
+++ main.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const electron = require('electron');
+
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow () {
+  app.setName('Material Todo');
+  // app.commandLine.appendSwitch('enable-speech-dispatcher');
+  if (process.platform === 'linux') {
+    app.commandLine.appendSwitch('enable-transparent-visuals');
+  }
+
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 350,
+    height:690,
+    frame: false,
+    alwaysOnTop: false,
+    center: true
+  });
+
+  mainWindow.setMenuBarVisibility(false);
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/dist/index.html');
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  app.quit();
+});
